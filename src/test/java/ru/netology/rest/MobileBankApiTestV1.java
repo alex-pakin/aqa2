fc4ef07e4d36ff63c7aa1d0f5666b90731342f66
alex-pakin <aleksei_pakin@mail.ru>
2022-07-20 21:27:27 +0300

    successful step 3

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,8 +1,12 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+
 
 class MobileBankApiTestV1 {
     @Test
@@ -16,6 +20,8 @@ class MobileBankApiTestV1 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(200);
+              .statusCode(200)
+              .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+
     }
 }
